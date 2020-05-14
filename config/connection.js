d39787bdab1eddc4b4d6a26ef19508874437e341
Jenelle Langford <jenellelangford@Jenelles-Air.home>
2020-05-14 13:28:02 -0500

    ready for heroku

--- config/connection.js
+++
@@ -1,9 +0,0 @@
-
-
-heroku: {
-  host: qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
-  port: 3306
-  user: mb0zcukxt9ff2dpu
-  password: fem9i5ip8xl069zb
-  database: ha4a7cr7o6vl4ygy
-}
--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-const PORT = process.env.PORT || 3306;
-const dbConfig = (process.env.NODE_ENV === 'production') ? dbConfig.heroku : config.db
+const PORT = process.env.PORT || 3002;
+// const dbConfig = (process.env.NODE_ENV === 'production') ? dbConfig.heroku : config.db
 
 // 
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +24,7 @@ app.set("view engine", "handlebars");
 
   const connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port: 3002,
     user: "jenelle",
     password: "jenelle123",
     database: "burgers_db",
@@ -44,28 +44,27 @@ connection.connect(function(err) {
 app.get('/', function(req,res) {
   connection.query('SELECT * FROM burgers;', function (err, response) {
     if (err) throw err; 
-    const haveBurger = [];
-    const yumBurger = [];
+    const orderedBurger = [];
+    const eatenBurger = [];
     for (var i =0; i < response.length; i++) {
       if (response[i].devoured) {
-        yum.push(response[i])
+        eatenBurger.push(response[i])
       }
     }
     
-    res.render('index', {haveBurger: haveBurger, yumBurger: yumBurger });
+    res.render('index', {orderedBurger: orderedBurger, eatenBurger: eatenBurger });
   });
   
 });
 
 app.post("/", function(req, res) {
-  connection.query("INSERT INTO burgers VALUES (?)", { burger_name: req.body.burger, devoured: false }, 
+  connection.query("INSERT INTO burgers VALUES (?)", { burger_name: req.body.burgers, devoured: false }, 
   {
     function (err, res) {
       if (err) throw err;
     }
-  };
+  });
     res.redirect("/")
-
 });
 
 
